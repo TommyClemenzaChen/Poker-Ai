edd177fe11435416e98ee2cdc0fa3d738bd22ef3
JunHayashida <g.jun375181031@gmail.com>
2023-07-24 00:03:42 -0700

    modify GlobalStyles.js for resutl page

--- poker-interface/GlobalStyles.js
+++ poker-interface/GlobalStyles.js
@@ -1,11 +1,20 @@
-// GlobalStyles.js
+
 /* fonts */
 export const FontFamily = {
     plusJakartaSansExtrabold: "Plus Jakarta Sans_extrabold",
     plusJakartaSansBold: "Plus Jakarta Sans_bold",
 };
+/* font sizes */
+export const FontSize = {
+    size_lg: 18,
+};
 /* Colors */
 export const Color = {
-    gray_100: "#fafafa",
-    white: "#fff",
+    gray: "#252525",
+    burlywood: "#eabf6f",
+    whitesmoke: "#f0f0f0",
+};
+/* Paddings */
+export const Padding = {
+    p_41xl: 60,
 };
