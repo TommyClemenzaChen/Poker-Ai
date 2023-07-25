de5132134263c339afd4744c5e7201ea23d85b8a
Pranav Vempati <pranav.k.vempati@gmail.com>
2023-07-24 19:57:14 -0700

    Documentation added to server.py

--- server.py
+++ server.py
@@ -4,13 +4,13 @@ from poker import PokerFlop, Player, get_action_from_input
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS(Cross Origin Requests)
-game_state = {} 
+game_state = {} # Game state as dictionary
 
 @app.route('/get_optimal_action', methods=['POST']) # Invoked on POST request to this route
 def get_optimal_action_route():
     '''
     Returns optimal action given data from frontend
-
+    (json): A JSON representation of the Optimal action and associated explanation
     '''
     data = request.get_json() # Frontend values are encapsulated in 'data'
 
@@ -19,7 +19,7 @@ def get_optimal_action_route():
     card2_value = data['card2_value'] # Second card value
     are_suited = data['are_suited'] 
     position = data['position'] # Position
-    min_bet = data['min_bet'] # Minimum bet(dynamic value)
+    min_bet = data['min_bet'] # Minimum bet to stay in the game
 
     game_state, optimal_action, explanation = get_action_from_input(player_name, card1_value, card2_value, are_suited, position, min_bet) # Retrieve optimal action, provided input
 
