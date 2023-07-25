6fb6758c145e7047872551f290d5f4b636dbe325
Manju Shettar <manjshettar@gmail.com>
2023-07-24 21:07:16 -0700

    error handling with tests

--- server.py
+++ server.py
@@ -14,15 +14,24 @@ def get_optimal_action_route():
     '''
     data = request.get_json() # Frontend values are encapsulated in 'data'
 
-    player_name = data['player_name'] # Player name
-    card1_value = data['card1_value'] # First Card Value
-    card2_value = data['card2_value'] # Second card value
-    are_suited = data['are_suited'] 
-    position = data['position'] # Position
-    min_bet = data['min_bet'] # Minimum bet to stay in the game
+    try:
+        player_name = data['player_name'] # Player name
+        if not isinstance(player_name, str):
+            raise ValueError("player_name must be a string")
+        card1_value = data['card1_value'] # First Card Value
+        card2_value = data['card2_value'] # Second Card Value
+        are_suited = data['are_suited'] # Are the cards suited
+        position = data['position'] # Position
+        min_bet = data['min_bet'] # Minimum Bet
 
-    game_state, optimal_action, explanation = get_action_from_input(player_name, card1_value, card2_value, are_suited, position, min_bet) # Retrieve optimal action, provided input
-
+        game_state, optimal_action, explanation = get_action_from_input(player_name, card1_value, card2_value, are_suited, position, min_bet) # Retrieve optimal action, provided input
+    except KeyError:
+        return jsonify({'error': 'Bad Request'}), 400
+    except ValueError:
+        return jsonify({'error': 'Bad Request'}), 400
+    except Exception as e:
+        return jsonify({'error': 'Internal Server Error'}), 500
+    
     return jsonify({'optimal_action': optimal_action.name, 'explanation': explanation}) # Return optimal action as JSON
 
 
--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -1,3 +1,8 @@
+# Run with: python -m unittest tests.integration_tests
+# This file contains integration tests to ensure functionality of the server.
+# These are integration tests, not unit tests. They are meant to test the functionality of the program as a whole,
+# not individual components or the accuracy of the program.
+
 import concurrent.futures
 import requests
 import time
@@ -6,19 +11,31 @@ from flask_testing import TestCase
 from flask import Flask
 import json
 
-# Assuming that your server.py file is in the same directory
 from server import app
+
+'''
+Server integration tests to verify that the server routing works as expected
+'''
 
 class ServerTest(TestCase):
     def create_app(self):
         return app
+    
+    def setUp(self):
+        # Code to setup before each test
+        print(f'\nRunning test: {self._testMethodName}...')
+
+    def tearDown(self):
+        # Code to cleanup after each test
+        print('Test complete.')
+        time.sleep(0.2)
 
     def test_all_possible_hands(self):
         # Define the set of all possible card values
         card_values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
         # Define the set of all possible positions
-        positions = ['Early', 'Middle', 'Late']
+        positions = ['Small Blind', 'Big Blind', 'UTG', 'Hijack', 'Cut-off', 'BTN']
 
         # Define the min bet
         min_bets = range(1, 6)
@@ -55,6 +72,26 @@ class ServerTest(TestCase):
 
                             # Check that the optimal action is one of the possible actions
                             self.assertIn(data['optimal_action'], ['FOLD', 'CALL', 'RAISE'])
+    
+    def test_empty_request_data(self):
+        response = self.client.post('/get_optimal_action', data=json.dumps({}), content_type='application/json')
+        self.assertEqual(response.status_code, 400)  # Replace 400 with the expected status code
 
+    def test_incomplete_request_data(self):
+        test_case = {'player_name': 'Player 1'}  # Incomplete data
+        response = self.client.post('/get_optimal_action', data=json.dumps(test_case), content_type='application/json')
+        self.assertEqual(response.status_code, 400)  # Replace 400 with the expected status code
+
+    def test_invalid_request_data(self):
+        test_case = {
+            'player_name': 123,  # Invalid data: player_name is not a string
+            'card1_value': '5',
+            'card2_value': 'J',
+            'are_suited': True,
+            'position': 'UTG',
+            'min_bet': 1
+        }
+        response = self.client.post('/get_optimal_action', data=json.dumps(test_case), content_type='application/json')
+        self.assertEqual(response.status_code, 400)  # Replace 400 with the expected status code
 if __name__ == '__main__':
     unittest.main(exit=False)
