163bb2a32cd12c74390544aaed5061d64db0e829
RahulNadkarni <95888811+RahulNadkarni@users.noreply.github.com>
2023-07-24 22:36:48 -0700

    starter

--- poker-interface/starter.js
+++ poker-interface/starter.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, ImageBackground, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const StarterPage = () => {
   const navigation = useNavigation();
-  const handlePlayButton = () => {
-    console.log('Study');
-    navigation.navigate('StudyPage');
-  };
 
-  const handleTrainButton = () => {
-    console.log('Train');
-    // Add logic for the "Train" button here -- route backend
+  const handlePlayButton = () => {
+    navigation.navigate('StudyPage');
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Poker AI</Text>
-      <TouchableOpacity style={styles.button} onPress={handlePlayButton}>
+      <View style={styles.borderTop} />
+      <View style={styles.borderLeft} />
+      <View style={styles.borderRight} />
+      <View style={styles.borderBottom} />
+
+      <ImageBackground
+        source={require('./assets/chip.png')} // Replace with the path to your poker chip PNG file
+        style={styles.button}
+        resizeMode="contain"
+        onPress={handlePlayButton}
+      >
         <Text style={styles.buttonText}>Play</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={handleTrainButton}>
-        <Text style={styles.buttonText}>Train</Text>
-      </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 };
@@ -30,28 +31,60 @@ const StarterPage = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: 'gray',
     justifyContent: 'center',
     alignItems: 'center',
   },
   title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
+    fontSize: 50,
+    fontFamily: 'Snell Roundhand',
+    color: '#ff6060', 
     marginBottom: 50,
   },
+  
   button: {
-    backgroundColor: 'orange',
-    borderRadius: 10,
-    paddingVertical: 15,
-    paddingHorizontal: 30,
-    marginBottom: 20,
+    width: 150,
+    height: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   buttonText: {
-    color: 'white',
+    color: 'red',
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  borderTop: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 10,
+    backgroundColor: '#ff6eff', // Neon pink color
+  },
+  borderLeft: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    width: 10,
+    backgroundColor: '#ff6eff', // Neon pink color
+  },
+  borderRight: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    width: 10,
+    backgroundColor: '#ff6eff', // Neon pink color
+  },
+  borderBottom: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 10,
+    backgroundColor: '#ff6eff', // Neon pink color
   },
 });
 
