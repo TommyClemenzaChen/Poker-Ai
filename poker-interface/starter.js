a006fc7f535aaed68d329a1128a4adbc651a7e3e
RahulNadkarni <95888811+RahulNadkarni@users.noreply.github.com>
2023-07-24 22:58:42 -0700

    correct

--- poker-interface/starter.js
+++ poker-interface/starter.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native'; // <-- Import useNavigation
+import { useNavigation } from '@react-navigation/native';
+
 const StarterPage = () => {
-  const navigation = useNavigation(); // <-- Get the navigation object
+  const navigation = useNavigation();
 
   const handlePlayButton = () => {
-    navigation.navigate('StudyPage'); // Navigate to the StudyPage screen
+    navigation.navigate('StudyPage');
   };
-
-  
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Poker AI</Text>
       <TouchableOpacity style={styles.button} onPress={handlePlayButton}>
-        <Text style={styles.buttonText}>Play</Text>
+        <Text style={styles.buttonText}>Play!</Text>
       </TouchableOpacity>
-      
     </View>
   );
 };
@@ -31,12 +29,13 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+    fontFamily: 'Plus Jakarta Sans_bold',
     color: 'white',
     marginBottom: 50,
   },
   button: {
     backgroundColor: '#EABF6F',
-    borderRadius: 10,
+    borderRadius: 100, // Set to a large value to make it circular
     paddingVertical: 15,
     paddingHorizontal: 30,
     marginBottom: 20,
@@ -46,6 +45,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
+    fontFamily: 'Plus Jakarta Sans_bold', // Set the font to cursive
   },
 });
 
